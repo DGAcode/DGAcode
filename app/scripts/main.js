75d3112425ff8fcb81022bf990af443691850645
Daniel Godigna <danielgodigna@gmail.com>
2016-07-14 02:29:42 +0200

    Activated service worker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,3 +12,13 @@ $(document).ready(function() {
     event.stopPropagation();
   });
 });
+
+if ('serviceWorker' in navigator) {
+ console.log('Service Worker is supported');
+ navigator.serviceWorker.register('../sw.js').then(function(reg) {
+   console.log(':^)', reg);
+   // TODO
+ }).catch(function(err) {
+   console.log(':^(', err);
+ });
+}
